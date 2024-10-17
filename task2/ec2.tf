b505000938d73f791ea67d73c40f8a9b9ff48d24
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-17 12:32:48 +0100

    update the ec2 instance for task2 with a key-pair, vpc and security group, using terraform

--- task2/ec2.tf
+++ task2/ec2.tf
@@ -1,8 +1,33 @@
 resource "aws_instance" "task_2_instance" {
-  ami           = "ami-0acc77abdfc7ed5a6"
-  instance_type = "t2.micro"
+  ami                    = "ami-0acc77abdfc7ed5a6"
+  instance_type          = "t2.micro"
+  key_name               = "TerraformTasks"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Task2Instance"
   }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "incoming_ssh" {
+  security_group_id = aws_security_group.allow_ssh.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
 }
