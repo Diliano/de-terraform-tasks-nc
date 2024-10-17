56586398e5ac4fc740fb680a49204fb27a02f55b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-17 10:31:13 +0100

    create an s3 bucket using terraform

--- task1/s3.tf
+++ task1/s3.tf
@@ -1,7 +1,12 @@
-resource "aws_s3_bucket" "demo_bucket" {
-  # TO BE IMPLEMENTED
+resource "aws_s3_bucket" "task_1_bucket" {
+  bucket_prefix = "task-1-bucket"
+
+  tags = {
+    Name        = "Task 1 bucket"
+    Environment = "Dev"
+  }
 }
 
-resource "aws_s3_object" "demo_object" {
-  # TO BE IMPLEMENTED
-}
+# resource "aws_s3_object" "demo_object" {
+#   # TO BE IMPLEMENTED
+# }
