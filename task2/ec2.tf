bd5e70729c1a82f704551e8088c61bee9a400c4a
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-18 11:32:02 +0100

    modify the startup script for task2 to create and run a simple web server on the ec2 instance

--- task2/ec2.tf
+++ task2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "task_2_instance" {
   ami                    = data.aws_ami.amzn-linux-2023-ami.id
   instance_type          = "t2.micro"
   key_name               = "TerraformTasks"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
   
   user_data = file("script.sh")
   user_data_replace_on_change = true
@@ -12,20 +12,36 @@ resource "aws_instance" "task_2_instance" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
 
   tags = {
-    Name = "allow_ssh"
+    Name = "allow_ssh_http"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "incoming_ssh" {
-  security_group_id = aws_security_group.allow_ssh.id
+  security_group_id = aws_security_group.allow_ssh_http.id
 
   cidr_ipv4   = "0.0.0.0/0"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "incoming_http" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "outgoing_http" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
--- task2/script.sh
+++ task2/script.sh
@@ -2,3 +2,11 @@
 
 echo "This file has been created and changed by a simple script!" > /home/ec2-user/file.txt
 echo "This file has been added in with the help of Terraform!" > /home/ec2-user/extra_file.txt
+
+sudo su
+dnf update -y
+dnf install httpd -y
+echo "<html><h1>Hello from the magical Terraform</h1></html>" > /var/www/html/index.html
+service httpd start
+chkconfig httpd on
+exit
